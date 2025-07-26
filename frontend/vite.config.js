4b8cf0cc68ea800436bc02ccb16a90a5ba29f555
DeekshaKV <deekshakv602@gmail.com>
2025-07-26 20:40:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  root: '.', // Set current folder as root
-  publicDir: 'public', // Ensures it loads from public/
-  build: {
-    outDir: 'build',
-    emptyOutDir: true,
-  },
-});
+"scripts": {
+  "start": "vite",
+  "build": "vite build",
+  "preview": "vite preview"
+}
